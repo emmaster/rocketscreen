0ac5621f0a1232addd436b5e101e1e4a1cfd3f0f
Oleksiy Kolesnikov <fullstability@gmail.com>
2016-07-01 19:21:32 +0300

    color transitions

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-
 $(function(){
-setInterval(function(){
-	alert("ok");
-}, 1000)	
-};)
+	colors = ['#641E16','#78281F','#512E5F','#4A235A','#154360','#1B4F72','#0B5345','#6E2C00','#1B2631','#17202A','black']
+	setInterval(function(){
+		randomElement = Math.floor(Math.random()*colors.length)
+		$('.fullhd').css('background',colors[randomElement])
+	}, 6000)	
+})
+
 
